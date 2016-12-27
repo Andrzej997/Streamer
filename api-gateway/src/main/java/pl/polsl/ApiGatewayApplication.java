a7630593806f48110d59f3a742059eed90380092
Andrzej997 <matisojka666@interia.pl>
2016-12-27 18:42:15 +0100

    test

--- api-gateway/src/main/java/pl/polsl/ApiGatewayApplication.java
+++ api-gateway/src/main/java/pl/polsl/ApiGatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @Controller
 @EnableZuulProxy
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
