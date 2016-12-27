f61a4eb5cec3986caff2a2a3dc28af7a2e4aa1ba
Andrzej997 <matisojka666@interia.pl>
2016-12-27 17:35:19 +0100

    fix

--- api-gateway/src/main/java/pl/polsl/ApiGatewayApplication.java
+++ api-gateway/src/main/java/pl/polsl/ApiGatewayApplication.java
@@ -3,18 +3,22 @@ package pl.polsl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableSidecar
+@Controller
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplateBuilder().build();
